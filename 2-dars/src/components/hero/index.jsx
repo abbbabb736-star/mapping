4a8347d327e18a4af8c432d35b4a25588a21dc2f
abbbabb736-star <abbbabb736@gmail.com>
2026-02-24 22:32:46 +0500

    fix bugs

---
+++ 2-dars/src/components/hero/index.jsx
@@ -0,0 +1,38 @@
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="container">
+        <div className="hero__wrapper">
+          <div className="hero__titles">
+            <p>Favorite brands</p>
+
+            <h1>
+              <span>An exciting place </span> <br />
+              for the whole family <br /> to shop.
+            </h1>
+
+            <p>
+              The standard chunk of Lorem Ipsum used since the 1500s is <br />{" "}
+              reproduced below for those interested.
+            </p>
+
+
+            <div className="hero__buttons">
+
+                <button >Shop Now</button>
+                <p>$ 249.99</p>
+                <p>$ 249.99</p>
+            </div>
+          </div>
+
+          <div className="hero__img">
+
+            <img src="/public/images/hero-img.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
